91d6216e2b6f2d91109d200544d1bd27ffd15c76
Henri Pietila <henrismail@gmail.com>
2019-06-21 09:58:43 -0700

    feat(sidebar): adds sidebar filter state and restore

--- src/SidebarList.js
+++ src/SidebarList.js
@@ -9,6 +9,8 @@ export default class SidebarList extends React.Component {
       sidebarData: [],
       filteredSidebarData: [],
       activeTags: [],
+      oldTags: [],
+      isFiltered: false,
       activeId: null,
       filter: true,
     }
@@ -22,19 +24,35 @@ export default class SidebarList extends React.Component {
     })
   }
 
-  componentDidUpdate(prevProps, prevState){
+  componentDidUpdate(prevProps, prevState) {
     const filter = this.props.layoutSelectors.currentFilter()
     if (prevState.filter !== filter) {
       this.updatefilteredSidebarData(filter)
     }
 
   }
-  updatefilteredSidebarData(filter){
+  updatefilteredSidebarData(filter) {
+    // on a search all tags are active, when the search is removed we go back to old state
+    if (!this.state.isFiltered && filter !== "") {
+      const newActiveTags = this.state.sidebarData.map((sidebarItem, tag) => tag)
+      this.setState({
+        oldTags: this.state.activeTags,
+        activeTags: newActiveTags,
+        isFiltered: true
+      })
+    }
+    else if (this.state.isFiltered && filter === "") {
+      this.setState({
+        activeTags: this.state.oldTags,
+        isFiltered: false
+      })
+    }
+
     const filteredSidebarData = this.props.fn.opsFilter(this.state.sidebarData, filter)
-    this.setState({filter, filteredSidebarData})
+    this.setState({ filter, filteredSidebarData })
   }
 
-  buildSidebarURL (string) {
+  buildSidebarURL(string) {
     return string
       .replace(/\//, '')
       .replace(/({|})/g, '_')
@@ -42,29 +60,25 @@ export default class SidebarList extends React.Component {
       .replace(/-/, '_')
   }
 
-  moveToAnchor (destination) {
+  moveToAnchor(destination) {
     window.scrollTo(0, destination.offsetTop - 120)
   }
 
-  isIdActive (id) {
+  isIdActive(id) {
     return this.state.activeId === id
   }
 
-  isTagActive (tag) {
+  isTagActive(tag) {
     return this.state.activeTags.includes(tag)
   }
 
-  ifActive (bool) {
-    return bool? "active": ""
+  ifActive(bool) {
+    return bool ? "active" : ""
   }
 
-  ifSubmenuActive() {
-
-  }
-
-  sidebarAnchorClicked (tag, id) {
-    this.setState({activeTags: [ ...this.state.activeTags, tag] })
-    this.setState({activeId: id})
+  sidebarAnchorClicked(tag, id) {
+    this.setState({ activeTags: [...this.state.activeTags, tag] })
+    this.setState({ activeId: id })
     this.props.layoutActions.show(["operations-tag", tag], true)
     this.props.layoutActions.show(["operations", tag, id], true)
     const idUrl = this.buildSidebarURL(id)
@@ -74,40 +88,39 @@ export default class SidebarList extends React.Component {
     this.moveToAnchor(anchor)
   }
 
-  subMenuClicked (tag) {
+  subMenuClicked(tag) {
     if (this.isTagActive(tag)) {
-      this.setState({activeTags: this.state.activeTags.filter(activeTag => activeTag !== tag) })
+      this.setState({ activeTags: this.state.activeTags.filter(activeTag => activeTag !== tag) })
       return
     }
-    this.setState({ activeTags: [ ...this.state.activeTags, tag ] })
+    this.setState({ activeTags: [...this.state.activeTags, tag] })
   }
 
   render() {
     return (
       <div className="spec sidebar-list" id="spec-sidebar-list">
-      <ul>
-        <li className="list-title">Resources</li>
+        <ul>
+          <li className="list-title">Resources</li>
 
-        { this.state.filteredSidebarData.map( (sidebarItem, tag) =>
-          <li className={ "submenu " + this.ifActive(this.state.filter.length || this.isTagActive(tag)) } >
-          <span className="submenu-title" onClick={() => this.subMenuClicked(tag)}>{tag}</span>
-          <ul className="submenu-items">
+          {this.state.filteredSidebarData.map((sidebarItem, tag) =>
+            <li className={"submenu " + this.ifActive(this.isTagActive(tag))} >
+              <span className="submenu-title" onClick={() => this.subMenuClicked(tag)}>{tag}</span>
+              <ul className="submenu-items">
+                {sidebarItem.get("operations").map(op =>
+                  <div>
+                    <li className={"method " + this.ifActive(this.isIdActive(op.get("id")))} >
+                      <a onClick={() => this.sidebarAnchorClicked(tag, op.get("operation").get("operationId"))} className={"method-" + op.get("method")}>
+                        {"  " + op.get("operation").get("summary")}
+                      </a>
+                    </li>
 
-            { sidebarItem.get("operations").map(op =>
-              <div>
-                <li className={ "method " + this.ifActive(this.isIdActive(op.get("id"))) } >
-                  <a onClick={() => this.sidebarAnchorClicked(tag, op.get("operation").get("operationId"))} className={"method-"+ op.get("method")}>
-                    {"  "  +  op.get("operation").get("summary")}
-                  </a>
-                </li>
+                  </div>)}
 
-              </div> )}
+              </ul>
+            </li>)}
 
-          </ul>
-        </li> )}
-
-      </ul>
-    </div>
+        </ul>
+      </div>
     )
   }
 }
