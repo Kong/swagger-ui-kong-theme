2f822b8aab0d25ca5d1289821be7c6801fc550c7
David Ma <davidma415@gmail.com>
2022-04-08 10:32:30 -0600

    fix: make responses section tabbable and readable (TDX-1890)

---
+++ src/components/HighlightCode.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react"
+import { highlight } from "../helpers/helpers"
+import saveAs from "js-file-download"
+
+export default class HighlightCode extends Component {
+  componentDidMount() {
+    highlight(this.el)
+  }
+
+  componentDidUpdate() {
+    highlight(this.el)
+  }
+
+  initializeComponent = (c) => {
+    this.el = c
+  }
+
+  downloadText = () => {
+    saveAs(this.props.value, this.props.fileName || "response.txt")
+  }
+
+  preventYScrollingBeyondElement = (e) => {
+    const target = e.target
+
+    var deltaY = e.nativeEvent.deltaY
+    var contentHeight = target.scrollHeight
+    var visibleHeight = target.offsetHeight
+    var scrollTop = target.scrollTop
+
+    const scrollOffset = visibleHeight + scrollTop
+
+    const isElementScrollable = contentHeight > visibleHeight
+    const isScrollingPastTop = scrollTop === 0 && deltaY < 0
+    const isScrollingPastBottom = scrollOffset >= contentHeight && deltaY > 0
+
+    if (isElementScrollable && (isScrollingPastTop || isScrollingPastBottom)) {
+      e.preventDefault()
+    }
+  }
+
+  render () {
+    let { value, className, downloadable } = this.props
+    className = className || ""
+
+    return (
+      <div className="highlight-code" tabIndex={0}>
+        { !downloadable ? null :
+          <div className="download-contents" onClick={this.downloadText}>
+            Download
+          </div>
+        }
+        <pre
+          ref={this.initializeComponent}
+          onWheel={this.preventYScrollingBeyondElement}
+          className={className + " microlight"}
+          tabIndex="0">
+          {value}
+        </pre>
+      </div>
+    )
+  }
+}
---
+++ src/components/ModelWrapper.js
@@ -0,0 +1,34 @@
+import React, { Component, } from "react"
+
+export default class ModelWrapper extends Component {
+  onToggle = (name,isShown) => {
+    // If this prop is present, we'll have deepLinking for it
+    if(this.props.layoutActions) {
+      this.props.layoutActions.show(["models", name],isShown)
+    }
+  }
+
+  render(){
+    let { getComponent, getConfigs } = this.props
+    const Model = getComponent("Model")
+
+    let expanded
+    if (this.props.layoutSelectors) {
+      // If this is prop is present, we'll have deepLinking for it
+      expanded = this.props.layoutSelectors.isShown(["models",this.props.name])
+    }
+
+    return (
+      <div className="model-box" tabIndex={0}>
+        <Model
+          { ...this.props }
+          getConfigs={ getConfigs }
+          expanded={expanded}
+          depth={ 1 }
+          onToggle={ this.onToggle }
+          expandDepth={ this.props.expandDepth || 0 }
+        />
+      </div>
+    )
+  }
+}
