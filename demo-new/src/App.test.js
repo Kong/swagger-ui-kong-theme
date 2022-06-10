1533983395aa42485cfb45b326a472825f1964bb
Pavol Kosc <pavol.kosc@hotovo.com>
2022-06-10 13:40:24 +0200

    feat/react-17-demoUpdate-restoredWorkingDemo

--- demo-new/src/App.test.js
+++ demo-new/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from 'demo-new/src/App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Kong/i);
   expect(linkElement).toBeInTheDocument();
 });
