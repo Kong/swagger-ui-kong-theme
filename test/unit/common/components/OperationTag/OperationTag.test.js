534b038c8c8624bc8238783c0231c6df64ddcdb4
Pavol Kosc <pavol.kosc@hotovo.com>
2022-06-17 11:59:40 +0200

    feat/kong-theme-unit-tests-addedTestStructures

--- test/unit/common/components/OperationTag/OperationTag.test.js
+++ test/unit/common/components/OperationTag/OperationTag.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import OperationTag from "components/OperationTag";
 
 describe('<OperationTag/>', () => {
+    let component;
+
     beforeAll(() => {
         const OperationTagProps = {};
         render(<OperationTag {...OperationTagProps}/>);
+    });
+
+    it('was rendered', () => {
+        expect(component).toBeInTheDocument();
+    })
+
+    afterAll(()=>{
+        component = null;
     })
 })
