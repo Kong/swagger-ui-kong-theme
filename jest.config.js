0e48d9c1a336f49ece4b3b1bab676de025eaa4ef
jozef_radonak <jozef.radonak@hotovo.com>
2022-06-19 21:56:05 +0200

    Updated tests config

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,15 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-    'verbose': true,
-    'bail': true,
-    'displayName': {
+    verbose: true,
+    bail: true,
+    displayName: {
         name: 'KongTheme Component',
         color: '#3a5fff'
     },
-    'moduleDirectories': ['node_modules'],
-    'roots': ['<rootDir>'],
-    'modulePaths': ['<rootDir>/src'],
-    'moduleFileExtensions': [
-        'js',
-        'json',
-    ],
-    'moduleNameMapper': {
-        '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
-        '@common/(.*)': '<rootDir>/../common/src/$1',
-        'test/(.*)': '<rootDir>/test/$1',
-        'components':'<rootDir>/src'
-    },
-    'testEnvironment': 'jsdom',
-    'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.js?$',
-    'reporters': ['default', ['jest-junit', {outputDirectory: 'test'}]]
+    modulePaths: ['<rootDir>/src/'],    
+    testEnvironment: 'jsdom',
+    reporters: ['default', ['jest-junit', {outputDirectory: 'test'}]]
 
 };
 
