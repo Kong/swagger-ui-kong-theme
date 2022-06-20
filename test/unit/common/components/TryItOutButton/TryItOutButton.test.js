f753583bca1d2a7c4d401694261811183a17fb4d
Pavol Kosc <pavol.kosc@hotovo.com>
2022-06-20 10:00:08 +0200

    feat/kong-theme-unit-tests-TryItOutButton

--- test/unit/common/components/TryItOutButton/TryItOutButton.test.js
+++ test/unit/common/components/TryItOutButton/TryItOutButton.test.js
@@ -3,47 +3,55 @@ import {render, screen} from '@testing-library/react';
 import user from '@testing-library/user-event';
 import TryItOutButton from 'components/TryItOutButton';
 
+
 describe('<TryItOutButton/>', () => {
     const onTryOutClickMock = jest.fn();
     const onCancelClickMock = jest.fn();
 
     describe('enabled: false', () => {
+        let tryOutBtn;
         beforeEach(() => {
             render(<TryItOutButton onTryoutClick={onTryOutClickMock} onCancelClick={onCancelClickMock} enabled={false}/>);
+            tryOutBtn = screen.getByRole('button');
         });
 
         it('was rendered', () => {
-            const tryOutBtn = screen.getByRole('button');
             expect(tryOutBtn).toBeVisible();
+            expect(tryOutBtn).toBeInTheDocument();
         });
 
+        it('has an aria label text: `Try sending an example request`', () => {
+            const text = screen.getByLabelText('Try sending an example request');
+            expect(text).toBeTruthy();
+        })
+
         it('tryOutButton functionality', async () => {
-            const tryOutBtn = screen.getByRole('button');
-            expect(tryOutBtn).toBeInTheDocument();
-            expect(tryOutBtn).toBeVisible();
             await user.click(tryOutBtn);
             expect(onTryOutClickMock).toHaveBeenCalledTimes(1);
         });
 
     });
-});
 
 
     describe('`enabled: true`', () => {
-        beforeAll(() => {
-            render(<TryItOutButton onTryoutClick={onTryOutClickMock} onCancelClick={onCancelClickMock}
-                                   enabled={false}/>);
+        let cancelBtn;
+        beforeEach(() => {
+            render(<TryItOutButton onTryoutClick={onTryOutClickMock} onCancelClick={onCancelClickMock} enabled={true}/>);
+            cancelBtn = screen.getByRole('button');
         });
 
         it('was rendered', () => {
-            expect(screen).toBeVisible();
+            expect(cancelBtn).toBeVisible();
+            expect(cancelBtn).toBeInTheDocument();
         });
 
-        it('tryOutButton functionality', () => {
-            const cancelBtn = screen.getByRole('img', {name: 'Cancel'});
-            expect(cancelBtn).toBeInTheDocument();
-            expect(cancelBtn).toBeVisible();
-            user.click(cancelBtn);
+        it('has an aria label text: `Cancel sending an example request`', () => {
+            const text = screen.getByLabelText('Cancel sending an example request');
+            expect(text).toBeTruthy();
+        })
+
+        it('tryOutButton functionality', async () => {
+            await user.click(cancelBtn);
             expect(onCancelClickMock).toHaveBeenCalledTimes(1);
         });
     });
