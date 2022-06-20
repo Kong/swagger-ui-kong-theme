b692ce9891625be6dbd0d1d72a9214debbf490eb
Pavol Kosc <pavol.kosc@hotovo.com>
2022-06-20 15:27:40 +0200

    feat/kong-theme-unit-tests-ContentType

--- test/unit/common/components/TryItOutButton/TryItOutButton.test.js
+++ test/unit/common/components/TryItOutButton/TryItOutButton.test.js
@@ -3,7 +3,6 @@ import {render, screen} from '@testing-library/react';
 import user from '@testing-library/user-event';
 import TryItOutButton from 'components/TryItOutButton';
 
-
 xdescribe('<TryItOutButton/>', () => {
     const onTryOutClickMock = jest.fn();
     const onCancelClickMock = jest.fn();
