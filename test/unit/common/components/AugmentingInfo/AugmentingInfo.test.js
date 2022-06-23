96096c9bba4e420cf1f7f365017d6895da6d5b55
Pavol Kosc <pavol.kosc@hotovo.com>
2022-06-23 12:34:46 +0200

    feat/kong-theme-unit-tests-augmentingInfoUpdate

--- test/unit/common/components/AugmentingInfo/AugmentingInfo.test.js
+++ test/unit/common/components/AugmentingInfo/AugmentingInfo.test.js
@@ -17,7 +17,10 @@ describe('<AugmentingInfo/>', () => {
         },
         url: 'MOCK_URL',
         basePath: 'MOCK_BASE_PATH',
-        externalDocsDescription: 'MOCK_externalDocsDescription',
+        externalDocs: {
+            description: 'MOCK_externalDocsDescription',
+            url: 'MOCK_EXTERNAL_URL'
+        },
         getComponent: jest.fn()
     };
 
@@ -62,7 +65,7 @@ describe('<AugmentingInfo/>', () => {
     });
 
     it('`ViewSpec` component is rendered', () => {
-       renderComponent(AugmentingInfoProps);
+        renderComponent(AugmentingInfoProps);
         const el = screen.getByText('View Raw');
         expect(el).toBeInTheDocument();
         expect(screen.getByTitle('0DC50B50-D286-4889-B1DA-07E43925811F@1.00x')).toBeInTheDocument();
@@ -74,7 +77,71 @@ describe('<AugmentingInfo/>', () => {
         expect(el).toBeTruthy();
     });
 
-    it('`url` section rendered', () => {
+    describe('`url` section rendered', () => {
+
+        it('has `url` text', () => {
+            renderComponent(AugmentingInfoProps);
+            const el = screen.getByText(AugmentingInfoProps.url);
+            expect(el).toBeTruthy();
+        });
+
+        it('has `a href` link', () => {
+            renderComponent(AugmentingInfoProps);
+            expect(screen.getByText(AugmentingInfoProps.url).closest('a')).toBeTruthy();
+        });
+
+        describe('`description` section', () => {
+            it('it has `description` classname', () => {
+                const container = renderComponent(AugmentingInfoProps);
+                const el = container.getElementsByClassName(`title`);
+                expect(el).toBeTruthy();
+            });
+            //todo: uncomment when mock component render fixed
+            xit('has `Markdown` component rendered', () => {
+                const container = renderComponent(AugmentingInfoProps);
+                const el = container.getElementsByTagName("Markdown");
+                expect(el).toBeTruthy();
+            });
+
+            describe('`terms of service` section', () => {
+                it('contains text `Terms of service`', () => {
+                    renderComponent(AugmentingInfoProps);
+                    expect(screen.getByText('Terms of service')).toBeInTheDocument();
+                });
+
+                it('contains `href`', () => {
+                    renderComponent(AugmentingInfoProps);
+                    expect(screen.getByText('Terms of service').closest('a')).toBeTruthy();
+                });
+            });
+
+            xit('has rendered `contact` section', () => {
+                renderComponent();
+                expect(el).toBeInTheDocument();
+            });
+
+            xit('has rendered `licence` section', () => {
+                renderComponent();
+                expect(el).toBeInTheDocument();
+            });
+
+            describe('`externalDocsUrl` section', () => {
+                let el;
+                beforeEach(() => {
+                    renderComponent(AugmentingInfoProps);
+                    el = screen.getByText(AugmentingInfoProps.externalDocs.description);
+                })
+
+                it('has externalDocs rendered', () => {
+                    const el = screen.getByText(AugmentingInfoProps.externalDocs.description);
+                    expect(el).toBeInTheDocument();
+                });
+
+                it('has `href` link', () => {
+                    expect(el.closest('a')).toBeTruthy();
+                })
+            })
+        })
     });
 
 });
