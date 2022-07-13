ff79aed0cc826f79be378125ea051f6022206aab
Pavol Kosc <pavol.kosc@hotovo.com>
2022-07-13 16:25:45 +0200

    removed unit test for demoApp

--- demo-new/test/utils/renderComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from "@testing-library/react";
-
-const renderComponent = (Component, props) => {
-  const { container } = render(<Component {...props} />);
-  return container;
-};
-
-export default renderComponent;
