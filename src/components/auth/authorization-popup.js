4cde6720c6e6674319adb8ae4f3e60ec5308454f
David Ma <40131297+davidma415@users.noreply.github.com>
2022-10-28 09:47:07 -0600

    feat: trap focus inside auth modal when open (TDX-2576) (#124)

--- src/components/auth/authorization-popup.js
+++ src/components/auth/authorization-popup.js
@@ -1,4 +1,5 @@
 import React from "react"
+import FocusTrap from "focus-trap-react"
 import PropTypes from "prop-types"
 
 export default class AuthorizationPopup extends React.Component {
@@ -13,10 +14,24 @@ export default class AuthorizationPopup extends React.Component {
     }
   }
 
-  componentDidMount() {
-    if (this.headerRef ) {
+  handleEscapeClose = (event) => {
+    if (event.code === 'Escape') {
+      this.close()
+    }
+  }
+
+  componentDidMount(){
+    if (this.headerRef) {
       setTimeout(() => {this.headerRef.focus()}, 100)
     }
+    // The keyup event listener is used on the document to capture all possible escape
+    // clicks inside the modal. Before, it was occasionally not firing maybe due to 
+    // a listener on one of the components inside.
+    document.addEventListener("keyup", this.handleEscapeClose)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keyup", this.handleEscapeClose)
   }
 
   render() {
@@ -24,42 +39,44 @@ export default class AuthorizationPopup extends React.Component {
     let definitions = authSelectors.shownDefinitions()
     const Auths = getComponent("auths")
     return (
-      <div className="dialog-ux" onKeyUp={({key}) => {
-        if (key === 'Escape') return this.close()
-      }}>
+      <div className="dialog-ux">
         <div className="backdrop-ux"></div>
-        <div className="modal-ux">
-          <div className="modal-dialog-ux">
-            <div className="modal-ux-inner">
-              <div className="modal-ux-header">
-                <h3
-                tabIndex="0"
-                ref={(_el) => this.headerRef = _el}
-                >Available authorizations</h3>
-                <button type="button" className="close-modal" onClick={ this.close }>
-                  <svg width="20" height="20">
-                    <use href="#close" xlinkHref="#close" />
-                  </svg>
-                </button>
-              </div>
-              <div className="modal-ux-content">
-                {
-                  definitions.valueSeq().map(( definition, key ) => {
-                    return <Auths key={ key }
-                      AST={AST}
-                      definitions={ definition }
-                      getComponent={ getComponent }
-                      errSelectors={ errSelectors }
-                      authSelectors={ authSelectors }
-                      authActions={ authActions }
-                      specSelectors={ specSelectors }
-                    />
-                  })
-                }
+        <FocusTrap>
+          <div className="modal-ux">
+            <div className="modal-dialog-ux">
+              <div className="modal-ux-inner">
+                <div className="modal-ux-header">
+                  <h3
+                    tabIndex="0"
+                    ref={(_el) => this.headerRef = _el}
+                  >
+                    Available authorizations
+                  </h3>
+                  <button type="button" className="close-modal" onClick={ this.close }>
+                    <svg width="20" height="20">
+                      <use href="#close" xlinkHref="#close" />
+                    </svg>
+                  </button>
+                </div>
+                <div className="modal-ux-content">
+                  {
+                    definitions.valueSeq().map(( definition, key ) => {
+                      return <Auths key={ key }
+                        AST={AST}
+                        definitions={ definition }
+                        getComponent={ getComponent }
+                        errSelectors={ errSelectors }
+                        authSelectors={ authSelectors }
+                        authActions={ authActions }
+                        specSelectors={ specSelectors }
+                      />
+                    })
+                  }
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </FocusTrap>
       </div>
     )
   }
